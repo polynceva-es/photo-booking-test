abb89ad247dd06cd6281de5d7ed62ade5beb6995
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-08-18 23:47:09 +0300

    add validation

--- src/components/pages/Signin.js
+++ src/components/pages/Signin.js
@@ -5,21 +5,53 @@ import FormAuth from "../FormAuth";
 import AuthIntegration from "../AuthIntegration";
 import useValidation from "../../hooks/useValidation";
 
-
 function Signin() {
   const location = useLocation();
   const { values, errors, onChange, resetValidation, isFormValid } =
-  useValidation();
+    useValidation();
 
   React.useEffect(() => {
     console.log(new URLSearchParams(location.hash).get("access_token"));
     console.log(new URLSearchParams(location.search).get("code"));
+    resetValidation({ email: "", password: "" });
   }, []);
   return (
     <div className="flex">
       <h1>Войти в аккаунт</h1>
-      <AuthIntegration/>
-      <FormAuth child={<></>} buttonTitle={'Войти'}/>
+      <AuthIntegration />
+      <FormAuth
+        child={
+          <>
+            <label htmlFor="reg-email" className="">
+              Email
+              <input
+                className=""
+                id="reg-email"
+                name="email"
+                type="email"
+                onChange={onChange}
+                value={values.email || ""}
+              />
+              <span className="">{errors.email || ""}</span>
+            </label>
+            <label htmlFor="reg-pass" className="">
+              Пароль
+              <input
+                className=""
+                id="reg-pass"
+                name="password"
+                type="password"
+                onChange={onChange}
+                value={values.password || ""}
+                required
+              />
+              <span className="">{errors.password || ""}</span>
+            </label>
+          </>
+        }
+        buttonTitle={"Войти"}
+        isFormValid={isFormValid}
+      />
       {/* передать пропсы */}
     </div>
   );
--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -6,12 +6,16 @@ import useValidation from "../../hooks/useValidation";
 
 function Signup() {
   const { values, errors, onChange, resetValidation, isFormValid } =
-  useValidation();
+    useValidation();
   const title = `Присоединиться как ${true ? "заказчик" : "специалист"}`;
+  React.useEffect(() => {
+    resetValidation({ name: "", surname: "", email: "", password: "" });
+  }, []);
+
   return (
     <div className="flex">
       <h1>{title}</h1>
-      <AuthIntegration/>
+      <AuthIntegration />
       <FormAuth
         child={
           <>
@@ -45,9 +49,35 @@ function Signup() {
               />
               <span className="">{errors.surname || ""}</span>
             </label>
+            <label htmlFor="reg-email" className="">
+              Email
+              <input
+                className=""
+                id="reg-email"
+                name="email"
+                type="email"
+                onChange={onChange}
+                value={values.email || ""}
+              />
+              <span className="">{errors.email || ""}</span>
+            </label>
+            <label htmlFor="reg-pass" className="">
+              Пароль
+              <input
+                className=""
+                id="reg-pass"
+                name="password"
+                type="password"
+                onChange={onChange}
+                value={values.password || ""}
+                required
+              />
+              <span className="">{errors.password || ""}</span>
+            </label>
           </>
         }
-        buttonTitle={'Присоединиться'}
+        buttonTitle={"Присоединиться"}
+        isFormValid={isFormValid}
       />
     </div>
   );
