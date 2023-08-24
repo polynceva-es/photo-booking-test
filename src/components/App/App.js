4675311c9cec534f565a09254db04da85f9c0ab5
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-08-24 09:17:49 +0300

    deleted files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,15 +14,7 @@ import {
 import { Signin } from '../../pages/Signin/Signin';
 import { Signup } from '../../pages/Signup/Signup';
 import { ResetPassword } from '../../pages/ResetPassword/ResetPassword';
-import { CatalogExecutors } from '../../pages/CatalogExperts/CatalogExperts';
-import { Profile } from '../../pages/Profile/Profile';
 import { Landing } from '../../pages/Landing/Landing';
-import { ClientAccount } from '../../pages/ClientAccount/ClientAccount';
-import { ClientOrders } from '../../pages/ClientOrders/ClientOrders';
-import { ExecutorAccount } from '../../pages/ExpertAccount/ExpertAccount';
-import { ExecutorOrders } from '../../pages/ExpertOrders/ExpertOrders';
-import { ExecutorRatings } from '../../pages/ExpertRatings/ExpertRatings';
-import { OrderServices } from '../../pages/OrderServices/OrderServices';
 import { ClientChat } from '../../pages/ClientChat/ClientChat';
 import { ExecutorChat } from '../../pages/ExpertChat/ExpertChat';
 import { Page404 } from '../../pages/404/404';
@@ -179,38 +171,6 @@ export function App() {
             onSubmitSendEmailToResetPassword={onSubmitSendEmailToResetPassword}
           />
         }
-      />
-      <Route
-        path="/catalog"
-        element={<CatalogExecutors />}
-      />
-      <Route
-        path="/card/:id"
-        element={<Profile />}
-      />
-      <Route
-        path="/client/:id"
-        element={<ClientAccount />}
-      />
-      <Route
-        path="/client/:id/orders"
-        element={<ClientOrders />}
-      />
-      <Route
-        path="/expert/:id"
-        element={<ExecutorAccount />}
-      />
-      <Route
-        path="/expert/:id/orders"
-        element={<ExecutorOrders />}
-      />
-      <Route
-        path="/expert/:id/ratings"
-        element={<ExecutorRatings />}
-      />
-      <Route
-        path="/order-service"
-        element={<OrderServices />}
       />
       <Route
         path="/client/:id/chat"
