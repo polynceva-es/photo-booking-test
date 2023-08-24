a50d54588f876f9f17b47d4b642c2a766a27a009
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-08-24 12:32:07 +0300

    fix

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,23 +1,7 @@
 import { useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
 import "./Landing.css";
 export const Landing = () => {
   const navigate = useNavigate();
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors, isValid },
-  } = useForm({mode: 'onChange'});
-
-  const onSubmit = (data) => {
-    console.log(isValid);
-    console.log(data);
-    reset();
-    console.log("reset")
-
-  };
-
 
   return (
     <div className={"landing-container"}>
